e4474805dd6b43aecb3107f6d6c7958f5bd8c910
francis-auka <francisauka7@gmail.com>
2025-07-18 11:29:46 +0300

    Update API baseURL to deployed backend

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://week-7-devops-deployment-assignment-dnr6.onrender.com/api',
 });
 
 export const fetchPosts = () => API.get('/posts');
